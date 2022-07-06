03fbafd23a9ef6ff13ba7b940485ad2a667178e2
ahmad <qudah729@gmail.com>
2022-07-06 13:22:06 +0300

    calculations app

---
+++ app/src/main/java/com/example/sum2nums/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.sum2nums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btResult)
+        val edtxt1: EditText = findViewById(R.id.ednum1)
+        val edtxt2: EditText = findViewById(R.id.ednum2)
+        val resultTV: TextView = findViewById(R.id.txtresult)
+
+
+
+        var flag:String="Sum"
+        val spinnerval: Spinner=findViewById(R.id.spselect)
+        var options= arrayOf("Sum","Multiply")
+        spinnerval.adapter= ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,options)
+
+        spinnerval.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+               flag=options.get(p2)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
+
+
+            button.setOnClickListener{ view ->
+            var x: Int = edtxt1.text.toString().toInt();
+            var y: Int = edtxt2.text.toString().toInt();
+
+                if(flag=="Sum")
+            resultTV.text = sum(x,y).toString();
+                else
+            resultTV.text=multiply(x,y).toString();
+        }
+
+    }
+}
+
+public fun sum(a: Int, b: Int): Int {
+    return a+b;
+}
+
+
+public fun multiply(a: Int, b: Int): Int {
+    return a*b
+}
+
+
+
